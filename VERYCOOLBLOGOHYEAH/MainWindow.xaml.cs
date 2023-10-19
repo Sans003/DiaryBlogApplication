232ca2a6d97b6472788b91cf934e1f1c46cc9e21
Sans003 <peanutstv1@gmail.com>
2023-10-19 13:19:00 +0200

    Added everything

--- VERYCOOLBLOGOHYEAH/MainWindow.xaml.cs
+++ VERYCOOLBLOGOHYEAH/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +23,13 @@ namespace VERYCOOLBLOGOHYEAH
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DateTime postTime;
+        public int likes;
+        public string liked;
+        public string content;
+        public string title;
+        public bool Editing;
+        public int Id;
         public MainWindow()
         {
             InitializeComponent();
@@ -33,24 +43,10 @@ namespace VERYCOOLBLOGOHYEAH
                 switch (menuItem.Name)
                 {
                     case "Italic":
-                        if (BlogText.Selection.GetPropertyValue(FontStyleProperty).ToString() == "Italic")
-                        {
-                            BlogText.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Normal);
-                        }
-                        else
-                        {
-                            BlogText.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
-                        }
+                        ToggleStates(TextElement.FontStyleProperty, FontStyles.Normal, FontStyles.Italic);
                         break;
                     case "Bold":
-                        if (BlogText.Selection.GetPropertyValue(FontWeightProperty).ToString() == "Bold")
-                        {
-                            BlogText.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
-                        }
-                        else
-                        {
-                            BlogText.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
-                        }
+                        ToggleStates(TextElement.FontWeightProperty, FontWeights.Normal, FontWeights.Bold);
                         break;
                     case "FontSizeUp":
                         BlogText.Selection.ApplyPropertyValue(RichTextBox.FontSizeProperty, Math.Min((BlogText.Selection.GetPropertyValue(TextElement.FontSizeProperty) as double? ?? BlogText.FontSize) + 1.0, 30.0));
@@ -63,92 +59,192 @@ namespace VERYCOOLBLOGOHYEAH
                         break;
                 }
             }
+
+            void ToggleStates<T>(DependencyProperty property, T state1, T state2)
+            {
+                bool isState1 = BlogText.Selection.GetPropertyValue(property) is T t && t.Equals(state1);
+                BlogText.Selection.ApplyPropertyValue(property, isState1 ? state2 : state1);
+            }
         }
 
-        private void Filter_Selected(object sender, RoutedEventArgs e)
+        private void EditPost(object sender, RoutedEventArgs e)
         {
-            string Sorting = Filter.SelectedItem as string;
-            switch (Sorting)
+            foreach (var o in PostsList.SelectedItems.OfType<JournalEntry>())
             {
-                case "Name_plus":
-                    PostsList.Items.SortDescriptions.Clear();
-                    PostsList.Items.SortDescriptions.Add(
-                    new System.ComponentModel.SortDescription("Content",
-                         System.ComponentModel.ListSortDirection.Ascending));
-                    PostsList.Items.Refresh();
-                    break;
-                case "Name_minues":
-                    PostsList.Items.SortDescriptions.Clear();
-                    PostsList.Items.SortDescriptions.Add(
-                    new System.ComponentModel.SortDescription("Content",
-                         System.ComponentModel.ListSortDirection.Descending));
-                    PostsList.Items.Refresh();
-                    break;
-                case "Date_plus":
-                    PostsList.Items.SortDescriptions.Clear();
-                    PostsList.Items.SortDescriptions.Add(
-                    new System.ComponentModel.SortDescription("Date",
-                         System.ComponentModel.ListSortDirection.Ascending));
-                    PostsList.Items.Refresh();
-                    break;
-                case "Date_minues":
-                    PostsList.Items.SortDescriptions.Clear();
-                    PostsList.Items.SortDescriptions.Add(
-                    new System.ComponentModel.SortDescription("Date",
-                         System.ComponentModel.ListSortDirection.Descending));
-                    PostsList.Items.Refresh();
-                    break;
-                case "Likes_plus":
-                    PostsList.Items.SortDescriptions.Clear();
-                    PostsList.Items.SortDescriptions.Add(
-                    new System.ComponentModel.SortDescription("Likes",
-                         System.ComponentModel.ListSortDirection.Ascending));
-                    PostsList.Items.Refresh();
-                    break;
-                case "Likes_minues":
-                    PostsList.Items.SortDescriptions.Clear();
-                    PostsList.Items.SortDescriptions.Add(
-                    new System.ComponentModel.SortDescription("Likes",
-                         System.ComponentModel.ListSortDirection.Descending));
-                    PostsList.Items.Refresh();
-                    break;
+                PostTitle.Text = o.Title;
+                BlogText.Document.Blocks.Clear();
+                BlogText.AppendText(o.Content);
+                likes = o.Likes;
+                liked = o.Liked;
+                postTime = o.PostTime;
+                Id = o.ID;
+                Editing = true;
+                postBtn.Content = "Save Changes";
             }
+        }
+        private void Order_Selected(object sender, RoutedEventArgs e)
+        {
+            var (property, direction) = ((ComboBoxItem)Filter.SelectedItem).Name switch
+            {
+                "Name_minus" => ("Title", ListSortDirection.Descending),
+                "Date_plus" => ("PostTime", ListSortDirection.Ascending),
+                "Date_minus" => ("PostTime", ListSortDirection.Descending),
+                "Likes_plus" => ("Likes", ListSortDirection.Ascending),
+                "Likes_minus" => ("Likes", ListSortDirection.Descending),
+                _ => ("Title", ListSortDirection.Ascending),
+            };
+            PostsList.Items.SortDescriptions.Clear();
+            PostsList.Items.SortDescriptions.Add(new SortDescription(property, direction));
+            PostsList.Items.Refresh();
         }
 
         private void PostEntry(object sender, RoutedEventArgs e)
         {
             if (DataContext is JournalEntries journalEntries)
             {
-                JournalEntry entry = new JournalEntry();
-                entry.Title = PostTitle.Text;
-                entry.Content = new TextRange(BlogText.Document.ContentStart, BlogText.Document.ContentEnd).Text;
-                entry.PostTime = DateTime.Now;
-                if (entry.Likes > 1)
+                if (Editing)
                 {
-                    entry.Likes = 0;
+                    JournalEntry entry = journalEntries.Items.Single(i => i.ID == Id);
+                    entry.Title = PostTitle.Text;
+                    entry.Content = new TextRange(BlogText.Document.ContentStart, BlogText.Document.ContentEnd).Text;
+                    entry.PostTime = postTime;
+                    entry.ID = Id;
+                    entry.Likes = likes;
+                    entry.Liked = liked;
+                    PostsList.Items.Refresh();
+                    Editing = false;
+                    postBtn.Content = "Post";
                 }
-                entry.Liked = $"♡"; ///&#x2665
-                journalEntries.Items.Add(entry);
-                PostsList.Items.Refresh();
+                else
+                {
+                    JournalEntry entry = new JournalEntry();
+                    entry.Title = PostTitle.Text;
+                    entry.Content = new TextRange(BlogText.Document.ContentStart, BlogText.Document.ContentEnd).Text;
+                    entry.PostTime = DateTime.Now;
+                    if (entry.Likes > 1)
+                    {
+                        entry.Likes = 0;
+                    }
+                    entry.ID = journalEntries.Items.Select(x => x.ID).DefaultIfEmpty().Max() + 1;
+                    entry.Liked = $"♡"; ///&#x2665
+                    journalEntries.Items.Add(entry);
+                    PostsList.Items.Refresh();
+                }
             }
+            PostTitle.Clear();
+            BlogText.Document.Blocks.Clear();
         }
 
         private void LikePost(object sender, RoutedEventArgs e)
         {
-            
+            if (sender is Button { DataContext: JournalEntry entry })
+            {
+                if (entry.Liked == "♥")
+                {
+                    entry.Likes--;
+                    entry.Liked = "♡";
+                }
+                else
+                {
+                    entry.Likes++;
+                    entry.Liked = "♥"; ;
+                }
+                PostsList.Items.Refresh();
+            }
         }
-}
-public class JournalEntries
-{
-    public List<JournalEntry> Items { get; set; } = new List<JournalEntry>();
-}
-public class JournalEntry
-{
-    public DateTime PostTime { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
-    public int Likes { get; set; }
-    public string Liked { get; set; }
 
-}
+        private void clearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PostTitle.Clear();
+            BlogText.Document.Blocks.Clear();
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is JournalEntries journalEntries)
+            {
+                foreach (var o in PostsList.SelectedItems.OfType<JournalEntry>())
+                {
+                    Id = o.ID;
+                    JournalEntry entry = journalEntries.Items.Single(i => i.ID == Id);
+                    journalEntries.Items.Remove(entry);
+                    break;
+                }
+                PostsList.Items.Refresh();
+            }
+        }
+
+        private void SharePost(object sender, RoutedEventArgs e)
+        {
+            foreach (var n in PostsList.SelectedItems.OfType<JournalEntry>())
+            {
+                title = $"Title: {n.Title}";
+                content = n.Content;
+                likes = $"{n.Likes}";
+                postTime = $"{n.PostTime}";
+            }
+        }
+    }
+
+    public class JournalEntries
+    {
+        public ObservableCollection<JournalEntry> Items { get; } = new();
+    }
+
+    public class JournalEntry : INotifyPropertyChanged
+    {
+        private int id;
+        public int ID
+        {
+            get => id;
+            set => SetField(ref id, value);
+        }
+
+        private DateTime postTime;
+        public DateTime PostTime
+        {
+            get => postTime;
+            set => SetField(ref postTime, value);
+        }
+
+        private string title;
+        public string Title
+        {
+            get => title;
+            set => SetField(ref title, value);
+        }
+
+        private string content;
+        public string Content
+        {
+            get => content;
+            set => SetField(ref content, value);
+        }
+
+        private int likes;
+        public int Likes
+        {
+            get => likes;
+            set => SetField(ref likes, value);
+        }
+
+        private string liked;
+        public string Liked
+        {
+            get => liked;
+            set => SetField(ref liked, value);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
 }
